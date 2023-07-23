b9b396a9050d602e7661582eda2bf4353bfbd4d9
yogiwirmanda <wirmanda@gmail.com>
2023-07-23 13:15:29 +0700

    update config

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: '',
+    HOST: '103.186.0.18',
+    USER: 'wiwe-ubuntu',
+    PASSWORD: '123456',
     DB: 'appbayu',
     dialect: 'mysql',
     pool: {
@@ -11,3 +11,17 @@ module.exports = {
         idle: 10000,
     },
 }
+
+// module.exports = {
+//     HOST: 'localhost',
+//     USER: 'root',
+//     PASSWORD: '',
+//     DB: 'appbayu',
+//     dialect: 'mysql',
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000,
+//     },
+// }
